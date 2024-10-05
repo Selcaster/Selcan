64173f754b0bf5ba26a80e2d4844bcc7e9d3082e
Seljan Karimli <selcankk-ap215@code.edu.az>
2024-10-05 21:29:47 +0400

    Create oktyabr5.cs
    deyesen git islede bilmirem, allah mene sebir ve guc-quvvet versin.

---
+++ oktyabr5.cs
@@ -0,0 +1,28 @@
+#region Task1
+//İki ədəd verilir. Birinci ədədin ikinci ədəd qüvvətini hesablayan alqoritmin diaqramını qurun.
+//(məsələn: k və g verilir kᵍ print olunmalıdır.) ( loop ilə həll olunmalıdır.)
+//double k = 10, g = 12, answer = 1;
+//for (int i = 0; i < g; i++) answer *= k;
+//Console.WriteLine(answer);
+#endregion
+#region Task2
+//Verilmiş ədədin neçə mərtəbəli olduğunu print edən alqoritmin diaqramını qurun.
+//(məsələn: 321 ədədi 3 mərtəbəlidir, 23125 ədədi 5 mərtəbəlidir.)
+//long k = 21234245679886785, numberOfDigits = 0;
+//while (k > 0) 
+//{
+//    k /= 10;
+//    numberOfDigits++;
+//}
+//Console.WriteLine(numberOfDigits);
+#endregion
+#region Task3
+//Ədədlərdən ibarət arrayda neçə rəqəm olduğunu ekrana çap edin
+//(Məs: {1,77,6,14} arrayında 2 rəqəm var )
+//long[] numberarray = { 1, 77, 6, 14, 0, 9, 1 };
+//int numberOfDigits = 0;
+//for(int i = 0; i<numberarray.Length; i++)
+//    if(numberarray[i] >= 0 && numberarray[i] <= 9)
+//        numberOfDigits++;
+//Console.WriteLine(numberOfDigits);
+#endregion
