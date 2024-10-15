8d15d69aa87402f1b78dd30f826bf5d293ab6f5c
Seljan Karimli <selcankk-ap215@code.edu.az>
2024-10-15 07:21:53 +0400

    BUNLAR C# CLASSES

---
+++ Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Product
+    {
+        public string BrandName { get; set; }
+        public double Price { get; set; }
+    }
+    public Product(string BrandName, double Price) 
+    {
+        this.Brandname = BrandName;
+        this.Price = Price;
+    }
+}
